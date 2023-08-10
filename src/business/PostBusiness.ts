00d2993a593e1eb12a94e6b9b1dc26fb80fd35f7
Lucas-Louback <lucasmlouback99@gmail.com>
2023-08-10 17:01:17 -0300

    Endpoint create-post pronto

--- src/business/PostBusiness.ts
+++ src/business/PostBusiness.ts
@@ -1,11 +1,47 @@
 import { PostDatabase } from "../database/PostDatabase";
 import { IdGenerator } from "../services/IdGenerator";
 import { TokenManager } from "../services/TokenManager";
+import { CreatePostInputDTO, CreatePostOutputDTO } from "../dtos/post/createPost.dto";
+import { UnauthorizedError } from "../errors/UnauthorizedError";
+import { Post } from "../models/Post";
 
 export class PostBusiness {
-    constructor (
+    constructor(
         private postDatabase: PostDatabase,
-        private idGeneratos: IdGenerator,
+        private idGenerator: IdGenerator,
         private tokenManager: TokenManager
-    ) {}
+    ) { }
+
+    public createPost = async (input: CreatePostInputDTO): Promise<CreatePostOutputDTO> => 
+    {
+        const { content, token } = input
+
+        const payload = this.tokenManager.getPayload(token)
+
+        if (!payload) {
+            throw new UnauthorizedError()
+        }
+
+        const id = this.idGenerator.generate()
+        
+        const post = new Post(
+            id,
+            content,
+            0,
+            0,
+            new Date().toISOString(),
+            new Date().toISOString(),
+            payload.id,
+            payload.name
+        )
+
+        const postDB = post.toDBModel()
+
+        await this.postDatabase.insertPost(postDB)
+
+        const output: CreatePostOutputDTO = undefined
+
+        return(output)
+
+    }
 }
